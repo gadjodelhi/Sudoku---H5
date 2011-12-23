d718e265f96b5e72a801beabc9b74bb55ff81136
Jacek Karczmarczyk <gadjodelhi.github@karczmarczyk.pl>
2011-12-23 12:45:05 +0530

    gallery - working (almost) edit and add gallery commands

--- gallery/admin/php/commands/EditgalleryCommand.php
+++ gallery/admin/php/commands/EditgalleryCommand.php
@@ -4,10 +4,21 @@ require 'XMLFileCommand.php';
 
 class EditgalleryCommand extends XMLFileCommand {
 	public function execute($data) {
-		$gallery = $this->_xml->getElementById($data->originalid);
+		if (@$data->originalid) {
+			$gallery = $this->_xml->getElementById($data->originalid);
+		}
+		else {
+			$gallery = $this->_xml->createElement('gallery');
+			$this->_xml->documentElement->insertBefore($gallery, $this->_xml->documentElement->childNodes->length ? $this->_xml->documentElement->childNodes->item(0) : null);
+			$gallery->setAttribute('id', $data->id);
+			$gallery->setAttribute('path', $data->id);
+			$gallery->setAttribute('published', date("Y-m-d H:i:s"));
+			$gallery->setAttribute('index', 1);
+		}
 		$gallery->setAttribute('title', $data->title);
 		$gallery->setAttribute('description', $data->description);
-		$gallery->setAttribute('big', (isset($data->big) && $data->big) ? 'true' : 'false');
+		$gallery->setAttribute('big', $data->big);
+		$gallery->setAttribute('category', $data->category);
 	
 		$this->_export();
 		
@@ -18,18 +29,18 @@ class EditgalleryCommand extends XMLFileCommand {
 	}
 	
 	public function validate($data) {
-		if ($data->originalid && $data->originalid !== $data->id) {
+		if (@$data->originalid && $data->originalid !== $data->id) {
 			throw new Exception('ID change is not implemented yet');
 		}
 	
-		if ($data->originalid) {
+		if (@$data->originalid) {
 			$gallery = $this->_xml->getElementById($data->originalid);
 			if (!$gallery) {
 				throw new Exception('Gallery "' . $data->originalid . '" not found');
 			}
 		}
 
-		if ($data->id !== $data->originalid && $this->_xml->getElementById($data->id)) {
+		if ($data->id !== @$data->originalid && $this->_xml->getElementById($data->id)) {
 			throw new Exception('Gallery "' . $data->id . '" already exists');
 		}
 
